2b68c70deb9709044753c189f42afcb8ae770d80
Dendlian <86787641+Dendlian@users.noreply.github.com>
2021-08-13 20:37:21 +0900

    12

---
+++ Step1/Step1_1/20210813.cpp
@@ -0,0 +1,33 @@
+/*
+==Note==
+
+const int*p1 = &num1;
+// 저장하는 주소 변경 O
+// 간접 참조를 이용한 값 변경 X
+
+int* const p2 = &num2;
+// 저장하는 주소 변경 X
+// 간접 참조를 이용한 값 변경 O
+
+ * 다중포인터
+ // 포인터 변수의 주소를 저장하는 포인터
+*/
+
+#include<stdio.h>
+#include<string.h>
+#pragma warning(disable : 4996)
+
+int main() 
+{
+	int num = 10;
+	int* pNum = &num;
+	int* pNum2 = pNum;
+	int** ppNum = &pNum;
+
+	printf("%d, %d, %d, %d\n", **ppNum, *ppNum, ppNum, &ppNum);
+	printf("%d, %d, %d\n", *pNum, pNum, &pNum);
+	printf("%d, %d, %d\n", *pNum2, pNum2, &pNum);
+	printf("%d, %d\n", num, &num);
+
+	return 0;
+}

1fbce38c67426e49f39cf71d28ad46a1a18ab602
Dendlian <86787641+Dendlian@users.noreply.github.com>
2021-11-14 14:45:46 +0900

    10

--- Step2/Step2_2/Warrior.cpp
+++ Step2/Step2_2/Warrior.cpp
@@ -1,8 +1,7 @@
 #include "Warrior.h"
 
 // 생성자 : 부모 생성자
-// 부모의 메서드를 통하여  객체화를 초기화하는 기능
-
+// 부모의 메서드를 통하여 객체화를 초기화하는 기능
 
 Warrior::Warrior(string name, string weapon, float atk) : Job(name, weapon)
 {
